7e7cb464476f151c3c103500f7b422728d319a9c
kevingeorge0123 <66766337+kevingeorge0123@users.noreply.github.com>
2024-06-03 17:58:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ alert("HEllo");
 
 var numberofbutton = document.querySelectorAll(".drum").length
 
-for (var i=0, i <= numberofbutton, i++){
-    document.querySelectorAll("button")[i].addEventListener("click",alert1);
-}
+// for (var i=0, i <= numberofbutton, i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",alert1);
+// }
 
 // document.querySelector("button").addEventListener("click",alert1);
 
@@ -13,3 +13,53 @@ function alert1 (){
 }
 
 
+
+for (var i=0; i < numberofbutton; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",soundplay);
+}
+
+function soundplay(){
+    switch (this.innerText){
+        case "w": 
+        var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+        break;
+
+        case "a":
+            var audio =new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+
+        case "s":
+            var audio =new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio =new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio =new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio =new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio =new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        default:
+            console.log("try again");
+
+
+    };
+
+}
